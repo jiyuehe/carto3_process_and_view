9b356d5a6866213bf1def87a07c72be2dc07b37e
jiyuehe <jiyue_he@163.com>
2026-08-18 11:01:55 -0700

    .

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This is a user interface for processing and viewing Carto3 data.
 # Demonstration
 Screen capture of the user interface:  
 <img src="demo/demo.png" alt="Demo" width="800">
+
+# Setup
+Edit .vscode/settings.json: replace with your python virtual environment directory  
+Edit configuration.py: change the directory['data'] to your directory  
--- configuration.py
+++ configuration.py
@@ -39,7 +39,8 @@ def directory_setup():
     # directory folder
     directory = {}
     directory['home'] = script_dir
-    directory['data'] = Path('/home/j/Desktop/hdd/share_folder/carto3_files/data npz')
+    directory['data'] = Path('/home/j/Desktop/hdd/share_folder/carto3_files/data npz') # Jay's computer
+    # directory['data'] = Path('/home/j/Desktop/hdd/share_folder/carto3_files/data npz') # Ruhi
     directory['result'] = script_dir / 'result'
 
     (directory['result']).mkdir(parents=True, exist_ok=True) # create the folder if it does not exist
